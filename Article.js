1222193f429d6140c7fa1449d5e70c05e63e7c5c
appdeveloper9is <78894198+appdeveloper9is@users.noreply.github.com>
2021-09-03 18:49:34 +0500

    commit

---
+++ Article.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, StyleSheet, Platform, SafeAreaView } from "react-native";
+
+const Article = () => {
+
+
+  return (
+    <View>
+      Hello
+    </View>
+  );
+}
+export default Article;
---
+++ Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, StyleSheet, Platform, SafeAreaView } from "react-native";
+
+const Feed = () => {
+
+
+  return (
+    <View>
+      Check
+    </View>
+  );
+}
+export default Feed;
